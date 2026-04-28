8797ddcee91f1d172c8b099545fefec6a8c32921
a.latour <a.latour@sprinteins.com>
2026-04-28 14:31:45 +0200

    Neuer Fehler: Tippfehler beim Namen von Plays.json

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { statement } = require("./statement")
-const plays = require("./plays.json")
+const plays = require("./play.json")
 const invoices = require("./invoices.json")
 
 console.log(statement(invoices[0], plays));
